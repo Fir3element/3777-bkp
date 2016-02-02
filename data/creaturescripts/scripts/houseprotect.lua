0215063ede12b3328c310ba99ae7f461a1322bbc
Fir3element <gabriellinharesbernardes@gmail.com>
2016-02-02 17:18:50 -0200

    local..

--- data/creaturescripts/scripts/houseprotect.lua
+++ data/creaturescripts/scripts/houseprotect.lua
@@ -1,4 +1,4 @@
-function isInvited(houseId, playerName)
+local function isInvited(houseId, playerName)
 	if(string.find(string.lower(getHouseAccessList(houseId, 0x100)), playerName) or string.find(string.lower(getHouseAccessList(houseId, 0x101)), playerName)) then
 		return true
 	end
